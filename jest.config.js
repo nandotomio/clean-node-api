807bbea9446f9406a2c72f00746f0df61448f1de
Fernando Tomio <nandotomio@gmail.com>
2021-01-08 19:55:14 -0300

    chore: aupdate jest config file

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
   ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
