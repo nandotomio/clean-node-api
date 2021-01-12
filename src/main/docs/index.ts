a5f2536282136bba6d14835d19a982c0addd4f26
Fernando Tomio <nandotomio@gmail.com>
2021-01-12 14:16:31 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'NodeJS Rest API using TDD, Clean Architecture and Typescript',
+    version: '1.0.0'
+  }
+}
