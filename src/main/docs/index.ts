ae5ad7b09ca1e9daca44bfa434a4f89b174228c6
Fernando Tomio <nandotomio@gmail.com>
2021-01-12 16:28:56 -0300

    refactor: move docs to separate files

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,18 +1,6 @@
-import { loginPath, surveyPath, signUpPath, surveyResultPath } from './paths'
-import { badRequest, serverError, unauthorized, notFound, forbidden } from './components'
-import {
-  accountSchema,
-  errorSchema,
-  loginParamsSchema,
-  surveyAnswerSchema,
-  surveySchema,
-  surveysSchema,
-  apiKeyAuthSchema,
-  signUpParamsSchema,
-  addSurveyParamsSchema,
-  saveSurveyParamsSchema,
-  surveyResultSchema
-} from './schemas'
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -33,32 +21,7 @@ export default {
   },{
     name: 'Survey'
   }],
-  paths: {
-    '/login': loginPath,
-    '/signup': signUpPath,
-    '/surveys': surveyPath,
-    '/surveys/{surveyId}/result': surveyResultPath
-  },
-  schemas: {
-    error: errorSchema,
-    loginParams: loginParamsSchema,
-    account: accountSchema,
-    surveyAnswer: surveyAnswerSchema,
-    survey: surveySchema,
-    surveys: surveysSchema,
-    signUpParams: signUpParamsSchema,
-    addSurveyParams: addSurveyParamsSchema,
-    saveSurveyParams: saveSurveyParamsSchema,
-    surveyResult: surveyResultSchema
-  },
-  components: {
-    securitySchemes: {
-      apiKeyAuth: apiKeyAuthSchema
-    },
-    badRequest,
-    serverError,
-    unauthorized,
-    notFound,
-    forbidden
-  }
+  paths,
+  schemas,
+  components
 }
---
+++ src/main/docs/schemas.ts
@@ -0,0 +1,25 @@
+import {
+  accountSchema,
+  errorSchema,
+  loginParamsSchema,
+  surveyAnswerSchema,
+  surveySchema,
+  surveysSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas/'
+
+export default {
+  error: errorSchema,
+  loginParams: loginParamsSchema,
+  account: accountSchema,
+  surveyAnswer: surveyAnswerSchema,
+  survey: surveySchema,
+  surveys: surveysSchema,
+  signUpParams: signUpParamsSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema
+}
